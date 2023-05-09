55932989eb7b3d66ea78c563a6f23877774bf1dc
theiv <davib.g767@gmail.com>
2023-05-08 23:59:47 -0300

    add heapSort

--- AllSorts.java
+++ AllSorts.java
@@ -1,3 +1,7 @@
+/**
+ * Classe feita para reunir 4 implementações diferentes de algoritmos de sort
+ * (quickSort, mergeSort, heapSort e shellSort)
+ */
 public class AllSorts{
 
     private static void swap(int[] list, int positionA, int positionB){
@@ -8,6 +12,11 @@ public class AllSorts{
 
 
     // QUICK SORT ------------------------------------------------------------
+
+    /**
+     * Ordena um array usando o algoritmo quickSort
+     * @param list array para ser ordenado
+     */
     public static void quickSort(int[] list){
         if (list.length<2) return;
         quickSort(list, 0, list.length-1);
@@ -33,6 +42,10 @@ public class AllSorts{
 
 
     // MERGE SORT ------------------------------------------------------------
+    /**
+     * Ordena um array usando o algoritmo mergeSort
+     * @param list array para ser ordenado
+     */
     public static void mergeSort(int[] list){
         if (list.length<2) return;
         mergeSort(list, 0, list.length-1);
@@ -76,19 +89,81 @@ public class AllSorts{
             iteratorRight++;
             iteratorGlobal++;
         }
-        for(int i=0; i< subArrayLenght; i++) {
-            list[init + i] = temp[i];
-        }
+        System.arraycopy(temp, 0, list, init, subArrayLenght);
     }
 
 
     // HEAP SORT ------------------------------------------------------------
-    public static void heapSort(){
-        //implementação ...
+
+    public static class MaxHeap {
+        private int heapSize;
+        private final int[] list;
+
+        public MaxHeap(int[] list) {
+            this.list = list;
+            this.heapSize = this.list.length;
+            this.heapfy();
+        }
+
+        public void goDown(int currentPosition) {
+            int right = (currentPosition + 1) * 2; // posição do filho direito
+
+            if (right <= this.heapSize) {
+            //j=heapSize --> existe filho esquerdo, mas não existe filho direito
+            //j<heapSize --> exite filho esquerdo e filho direito
+
+
+                if (right < heapSize) { //existe filho direito?
+                    // se existir filho direito e esquerdo, comparar quem é maior
+                    if (this.list[right] > this.list[right-1])
+                        right++;
+                }
+
+                if (this.list[right - 1] > this.list[currentPosition]) {
+                    //fazer a troca com o maior filho e descer mais
+                    swap(this.list, currentPosition, right - 1);
+                    this.goDown(right -1);
+                }
+            }
+        }
+
+        public void heapfy() {
+            int mid = this.heapSize / 2; // depois de mid as posições são reservadas para folhas
+
+            for (int i = mid; i >=0 ; i--) { // descendo todos os valores do array do meio até 0
+                this.goDown(i);
+            }
+        }
+
+        public void pop() {
+            if (heapSize > 0) {
+                swap(this.list, 0, this.heapSize-1);
+                this.heapSize--;
+                this.goDown(0); // verificando a posição certa da nova raiz
+            }
+        }
+
+        public boolean isEmpty() {
+            return this.heapSize == 0;
+        }
+    }
+
+    /**
+     * Ordena um array usando o algoritmo heapSort
+     * @param list array para ser ordenado
+     */
+    public static void heapSort(int[] list){
+        if (list.length<2) return;
+        MaxHeap heap = new MaxHeap(list);
+        while (!heap.isEmpty())heap.pop();
     }
 
 
     // SHELL SORT ------------------------------------------------------------
+    /**
+     * Ordena um array usando o algoritmo shellSort
+     * @param list array para ser ordenado
+     */
     public static void shellSort(int[] list){
         if (list.length<2) return;
 
