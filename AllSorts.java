73b81b3905fd8758b40d058745b340c81363c3c5
Davi Barbosa <davib@edu.unifor.br>
2024-05-11 00:05:24 -0300

    Update AllSorts.java

--- AllSorts.java
+++ AllSorts.java
@@ -186,4 +186,42 @@ public class AllSorts{
         }
     }
 
+    // BUBBLE SORT ------------------------------------------------------------
+
+    public static void bubbleSort(int[] list){
+        int size = list.length;
+
+        for(int i=size-1; i >= 0; i--){
+            int numChanges = 0;
+
+            for(int j=0; j<i;j++ ){
+                if( (j+1 < size) && (list[j] > list[j+1]) ){
+                    swap(list, j, j+1);
+                    numChanges++;
+                }
+            }
+
+            if(numChanges == 0) break;
+        }
+    }
+
+
+    // SELECTION SORT ------------------------------------------------------------
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    
 }
